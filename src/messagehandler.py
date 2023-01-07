f41e09e57070d9467b893ef898d6e433871c9404
Roshan Karim <roshan@rkarim.xyz>
2023-01-07 01:12:52 -0500

    work on tmrw

--- src/messagehandler.py
+++ src/messagehandler.py
@@ -16,17 +16,25 @@ from pprint import pprint
 RUN_DAY = 5
 RUN_HOUR = 14
 
+sheet_controller = AttendanceSheetController()
+
 while True:
 
-    datetime_obj = datetime.today()
+    datetime_obj = datetime.now()
     day_of_week = datetime_obj.weekday()
+
+    forecasts = sheet_controller.get_forecasts_upcoming_week(datetime_obj)[User("forecast@ligerbots.org", "Liger", "Forecast")]
+    for attendance in forecasts.attendances:
+        if attendance.attendance:
+            pass
 
     while not getSuccess():
 
         if day_of_week == RUN_DAY and datetime_obj.hour == RUN_HOUR:
             response = Messenger.run()
             if response == 0:
-                setSuccess()
+
+
                 print("EXTREME SUCCESS")
             elif response == 1:
                 print("EXTREME FAILURE")
